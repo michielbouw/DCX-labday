6ab7d6de63cf06dfb4688d0c9319d79f0c8abd47
Eelco Bosklopper <eelco.bosklopper@capgemini.com>
2019-06-28 11:27:13 +0200

    Basic searchbar with autocomplete with stub

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Main msg="Amazing Environment"/>
+    <SearchBar />
   </div>
 </template>
 
 <script>
 import Main from './components/Main.vue'
+import SearchBar from './components/SearchBar.vue'
 
 export default {
   name: 'app',
   components: {
-    Main
+    Main,
+    SearchBar
   }
 }
 </script>
